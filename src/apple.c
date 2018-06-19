5dc2d9060ecdebbfcaafb9b84d117d6d46c4570c
Koyanagi Yugo <koyanagi.y.ab@is-mac44.edu.c.titech.ac.jp>
2018-06-19 17:23:28 +0900

    apple.c編集(2018.6.19)

--- src/apple.c
+++ src/apple.c
@@ -4,15 +4,41 @@ int n;
 int k;
 int A[100000];
 
-
+int p(int x,int A[]){
+    int i;
+    for(i = 0; i < n; i++){
+    int y = n;
+    while (y > 0) {
+        if (A[y]%x == 0) {
+            k = k-A[y]/x;
+            y = y-1;
+        }
+        else {
+            k = k-A[y]/x-1;
+            y = y-1;
+        }
+    }
+    }
+    return (int) k >= 0;
+}
 
 int main(){
-  int i, lb, ub;
+  int  i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
+      scanf("%d", &A[i]);
   }
-
-
+    lb = 0;
+    ub = n;
+    while (ub-lb > 1) {
+        int m = (lb+ub)/2;
+        if (p(A[m],A)) {
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    printf("%d\n",ub);
   return 0;
 }
