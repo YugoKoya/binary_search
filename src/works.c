bcd0bd065459018c8f05dd6d3ca18e51b1be05cd
Koyanagi Yugo <koyanagi.y.ab@is-mac18.edu.c.titech.ac.jp>
2018-08-03 12:49:45 +0900

    発展課題完成。

--- src/works.c
+++ src/works.c
@@ -4,6 +4,22 @@ int n;
 int k;
 int A[100000];
 
+int p(int m) {
+  int i, sum, x;
+  sum = 0;
+  x = 1;
+  for(i=0; i<n; i++) {
+    if (A[i] > m) return 0;
+    if (sum + A[i] <= m) {
+      sum = sum + A[i];
+    }
+    else {
+      x++;
+      sum = A[i];
+    }
+  }
+  return x <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,8 +27,8 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-  lb = 1;
-  ub = n;
+  lb = 0;
+  ub = 10000;
     while (ub-lb > 1){
         int m = (lb+ub)/2;
         if (p(m)) {
